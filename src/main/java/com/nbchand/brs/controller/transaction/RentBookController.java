f4220d97231b3b65a04c5bcd6a52b8e0c1ddb168
nbchand <narenchand02@gmail.com>
2022-04-02 11:08:49 +0545

    add comments in BookRentController

--- src/main/java/com/nbchand/brs/controller/transaction/RentBookController.java
+++ src/main/java/com/nbchand/brs/controller/transaction/RentBookController.java
@@ -6,6 +6,7 @@ import com.nbchand.brs.enums.RentType;
 import com.nbchand.brs.service.book.BookService;
 import com.nbchand.brs.service.bookTransaction.BookTransactionService;
 import com.nbchand.brs.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,24 +23,29 @@ import java.util.Date;
  */
 @Controller
 @RequestMapping("/rent")
+@RequiredArgsConstructor
 public class RentBookController {
 
-    private BookTransactionService bookTransactionService;
-    private MemberService memberService;
-    private BookService bookService;
+    private final BookTransactionService bookTransactionService;
+    private final MemberService memberService;
+    private final BookService bookService;
 
-    public RentBookController(BookTransactionService bookTransactionService, MemberService memberService, BookService bookService) {
-        this.bookTransactionService = bookTransactionService;
-        this.memberService = memberService;
-        this.bookService = bookService;
-    }
-
+    /**
+     * Displays landing page for book renting
+     * @param model
+     * @return respective web-page
+     */
     @GetMapping
     public String displayRentLanding(Model model) {
         model.addAttribute("bookTransactionDtoList", bookTransactionService.findTransactionsByRentType(RentType.RENT));
         return "rent/rentLanding";
     }
 
+    /**
+     * Displays book renting form
+     * @param model
+     * @return respective web-page
+     */
     @GetMapping("/create")
     public String displayRentForm(Model model) {
         model.addAttribute("memberDtoList", memberService.findAllEntities());
@@ -48,6 +54,14 @@ public class RentBookController {
         return "rent/rentForm";
     }
 
+    /**
+     * Creates book renting transaction
+     * @param bookTransactionDto
+     * @param bindingResult
+     * @param model
+     * @param redirectAttributes
+     * @return respective web-page
+     */
     @PostMapping
     public String addRent(@Valid @ModelAttribute BookTransactionDto bookTransactionDto,
                           BindingResult bindingResult,
@@ -75,6 +89,13 @@ public class RentBookController {
         return "redirect:/rent";
     }
 
+    /**
+     * Displays transaction form for editing purpose
+     * @param id
+     * @param model
+     * @param redirectAttributes
+     * @return respective web-page
+     */
     @GetMapping("/edit/{id}")
     public String displayEditForm(@PathVariable("id") Integer id, Model model, RedirectAttributes redirectAttributes) {
         ResponseDto transactionResponse = bookTransactionService.findEntityById(id);
@@ -88,6 +109,15 @@ public class RentBookController {
         return "redirect:/rent";
     }
 
+    /**
+     * Updates the book rent transaction
+     * @param id
+     * @param bookTransactionDto
+     * @param bindingResult
+     * @param model
+     * @param redirectAttributes
+     * @return
+     */
     @PutMapping("/{id}")
     public String updateRent(@PathVariable("id") Integer id,
                              @Valid @ModelAttribute BookTransactionDto bookTransactionDto,
@@ -122,6 +152,12 @@ public class RentBookController {
         return "redirect:/rent";
     }
 
+    /**
+     * Deletes the transaction of provided id
+     * @param id
+     * @param redirectAttributes
+     * @return respective web-page
+     */
     @DeleteMapping("/{id}")
     public String deleteTransaction(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes) {
         ResponseDto responseDto = bookTransactionService.deleteEntityById(id);
